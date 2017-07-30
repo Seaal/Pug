203769c3845db5cc5dacfa2bfedf2ca765d73be0
Seaal <Seaal@users.noreply.github.com>
2017-07-30 14:59:29 -0400

    Added coverage numbers for karma ran tests

--- src/Pug.Client/karma.conf.js
+++ src/Pug.Client/karma.conf.js
@@ -71,14 +71,27 @@ module.exports = function (config) {
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {},
+    preprocessors: {
+        "wwwroot/app/**/!(*.spec).js": ["coverage"]
+    },
 
 
     // test results reporter to use
     // possible values: "dots", "progress"
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ["progress"],
+    reporters: ["progress", "coverage", "remap-coverage"],
 
+    coverageReporter: {
+        type: "in-memory"
+    },
+
+    remapCoverageReporter: {
+        "text-summary": null
+    },
+
+    remapOptions: {
+        basePath: "./app"
+    },
 
     // web server port
     port: 9876,
