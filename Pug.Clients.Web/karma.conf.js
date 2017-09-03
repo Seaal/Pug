bb1a1edd41f8763c31a63592940f82a797575c09
Seaal <Seaal@users.noreply.github.com>
2017-09-03 14:54:30 -0400

    Added sourcemap support to karma for debugging

--- Pug.Clients.Web/karma.conf.js
+++ Pug.Clients.Web/karma.conf.js
@@ -75,7 +75,8 @@ module.exports = function (config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        "wwwroot/app/**/!(*.spec).js": ["coverage"]
+        "wwwroot/app/**/!(*.spec).js": ["coverage"],
+        "wwwroot/app/**/*.js": ["sourcemap"]
     },
 
 
