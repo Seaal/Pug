3d414339022dd5c03c6a8520ee151e81d6e0730e
Seaal <Seaal@users.noreply.github.com>
2018-03-26 20:16:33 -0400

    Temporary commit for localization changes

---
+++ Pug.Clients.Web/app/common/localization/localization.module.ts
@@ -0,0 +1,9 @@
+﻿import { NgModule } from "@angular/core";
+import { LocalizePipe } from "./localize.pipe";
+
+@NgModule({
+    declarations: [
+        LocalizePipe
+    ]
+})
+export class LocalizationModule { }
---
+++ Pug.Clients.Web/app/common/localization/localize.pipe.ts
@@ -0,0 +1,18 @@
+﻿import { Pipe, PipeTransform } from "@angular/core";
+
+import { TranslatePipe } from "@ngx-translate/core";
+import { Localization } from "./localization";
+
+@Pipe({
+    name: "Localize",
+    pure: false
+})
+export class LocalizePipe extends TranslatePipe implements PipeTransform { 
+
+    public transform(query: string, ...args: any[]): any {
+        const localization: Localization = query as any;
+
+        return super.transform(localization.key, localization.data);
+    }
+
+}
